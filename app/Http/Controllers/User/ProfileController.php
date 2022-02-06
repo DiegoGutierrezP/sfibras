ed0019245401df3ce3e64e381bbefa52fe50df7b
Diego <diego.sk75.dgp@gmail.com>
2022-02-06 00:03:35 -0500

    primer commit

---
+++ app/Http/Controllers/User/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateProfileRequest;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function update(UpdateProfileRequest $request){
+
+        auth()->user()->update($request->only('name','email'));
+
+        if($request->input('password')){
+            auth()->user()->update([
+                'password' => bcrypt($request->input('password'))
+            ]);
+        }
+
+        return redirect()->route('profile')->with('message','Perifl actualizado correctamente');
+    }
+}
