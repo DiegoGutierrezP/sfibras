c40ec9792bb1d0e20c1bb6efc8974e65e286ab54
diego <diego.gup.75@gmail.com>
2022-02-08 13:36:48 -0500

    footer v1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,23 +1,28 @@
-<section class="footer">
-    <div class="row">
-        <img src="{{Storage::url('cliente/sfibraslogo.png')}}" alt="">
-        <div class="menu-links">
-            <h2>Menu</h2>
-            <ul>
-                <li> <a href="">Home</a> </li>
-                <li> <a href="">Home</a> </li>
-                <li> <a href="">Home</a> </li>
-                <li> <a href="">Home</a> </li>
-                <li> <a href="">Home</a> </li>
-            </ul>
+
+<footer>
+    <section class="footer1">
+        <div class="logo">
+            <img src="{{Storage::url('cliente/sfibraslogo.png')}}" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi repudiandae
+                temporibus neque alias excepturi voluptatem maiores possimus debitis, molliti
+                a magnam nihil ipsa amet, fuga illum quasi cupiditate ea laudantium! Dolorem.</p>
         </div>
-        <div class="contacto">
-            <h2>Contacto</h2>
-            <label><i class="fa-solid fa-envelope"></i> sfibras@gmail.com</label>
-            <label><i class="fa-solid fa-phone"></i> 9946871799</label>
-        </div>
-    </div>
+        <ul class="menu-links">
+            <h3>Menu</h3>
+            <li><a href="">Home</a></li>
+            <li><a href="">Nosotros</a></li>
+            <li><a href="">Servicios</a></li>
+            <li><a href="">Proyectos</a></li>
+            <li><a href="">Cotizar</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+        <ul class="contacto">
+            <h3>Contacto</h3>
+            <li>94687199</li>
+            <li><a href="">sfibrasje@gmail.com</a></li>
+        </ul>
+    </section>
     <div class="footer2">
-        <p class="copy">&copy; Sfibras - Todos los derechos reservados. | Diseñado DiegoGP</p>
+        <p>Sfibras todos los derechos reservados | By DiegoGP</p>
     </div>
-</section>
+</footer>
